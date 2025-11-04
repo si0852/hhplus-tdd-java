9181b4600e5bdf830d128a3a593854c28c1ec6e8
sihyun <bags2578@gmail.com>
2025-11-04 20:31:10 +0900

    REFACTOR: 포인트조회 테스트 파일 이름 변경

--- src/test/java/io/hhplus/tdd/point/PointCheckTest.java
+++ src/test/java/io/hhplus/tdd/point/PointCheckTest.java
@@ -1,22 +1,19 @@
 package io.hhplus.tdd.point;
 
 import io.hhplus.tdd.database.UserPointTable;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class PointServiceTest {
+public class PointCheckTest {
 
     @Mock
     private UserPointTable userPoint;
